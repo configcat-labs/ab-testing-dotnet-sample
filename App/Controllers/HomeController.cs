739a7a7a953a1e0a3d0d8500c2480d0748a7c7d7
Emil Kovacevic <77700903+emilkovacevic@users.noreply.github.com>
2023-01-12 19:58:11 +0100

    Update HomeController.cs

--- App/Controllers/HomeController.cs
+++ App/Controllers/HomeController.cs
@@ -1,87 +1,74 @@
-﻿using ConfigCat.Client;
+using ConfigCat.Client;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
 using Newtonsoft.Json;
 
-namespace AB_test_app.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly IConfigCatClient configCatClient;
+namespace AB_test_app.Controllers {
+  public class HomeController: Controller {
+    private readonly IConfigCatClient configCatClient;
 
-        public HomeController(IConfigCatClient configCatClient)
-        {
-            this.configCatClient = configCatClient;
-        }
+    public HomeController(IConfigCatClient configCatClient) {
+      this.configCatClient = configCatClient;
+    }
 
-        // creating a user
-        readonly User userObject = new User("uid-1234-12-55");
-        public IActionResult Index()
-        {
-            // assigning the flag value depending on the ConfigCat response
-            bool isFlagOn = this.configCatClient.GetValue("addtocartflag", false, userObject);
+    // creating a user
+    readonly User userObject = new User("UID-132dadss4-12-55");
+    public IActionResult Index() {
+      // assigning the flag value depending on the ConfigCat response
+      bool isFlagOn = this.configCatClient.GetValue("addtocartflag", false, userObject);
 
-            ViewBag.Style = GetStyle(isFlagOn);
-            //ViewBag.flag = isFlagOn;
-            return View();
-        }
+      ViewBag.Style = GetStyle(isFlagOn);
+      //ViewBag.flag = isFlagOn;
+      return View();
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> AddToCartForm()
-        {
-            // assigning the flag value depending on the ConfigCat response
-            bool isFlagOn = this.configCatClient.GetValue("addtocartflag", false, userObject);
+    [HttpPost]
+    public async Task < IActionResult > AddToCartForm() {
+      // assigning the flag value depending on the ConfigCat response
+      bool isFlagOn = this.configCatClient.GetValue("addtocartflag", false, userObject);
 
-            await LogEventToAmplitude(isFlagOn, userObject);
+      await LogEventToAmplitude(isFlagOn, userObject);
 
-            // Redirect back to the same page
-            return RedirectToAction("Index");
-        }
+      // Redirect back to the same page
+      return RedirectToAction("Index");
+    }
 
-        private string GetStyle(bool isFlagOn
-        
-            if (isFlagOn)
-            {
-                // setting the new style of the add-to-chart submit form
-                return "border: 1px solid green; color:green";
+    private string GetStyle(bool isFlagOn
+
+      if (isFlagOn) {
+        // setting the new style of the add-to-chart submit form
+        return "border: 1px solid green; color:green";
+      } else {
+        return "";
+      }
+    }
+
+    private async Task LogEventToAmplitude(bool isFlagOn, User userObject) {
+      // HTTP POST request body:
+      var data = new {
+        api_key = "YOUR-AMPLITUDE-API-KEY",
+          events = new [] {
+            new {
+              user_id = userObject.Identifier,
+                event_type = isFlagOn ? "Variant Cart Button" : "Original Cart Button",
             }
-            else
-            {
-                return "";
-            }
-        }
+          }
+      };
 
-        private async Task LogEventToAmplitude(bool isFlagOn, User userObject)
-        {
-            // HTTP POST request body:
-            var data = new
-            {
-                api_key = "YOUR-AMPLITUDE-API-KEY",
-                events = new[]
-                {
-                    new
-                    {
-                        user_id = userObject.Identifier,
-                        event_type = isFlagOn ? "Variant Cart Button" : "Original Cart Button",
-                    }
-                }
-            };
+      // Create an HttpClient
+      var client = new HttpClient();
 
-            // Create an HttpClient
-            var client = new HttpClient();
+      // Send POST request to Amplitude
+      var response = await client.PostAsync(
+        "https://api.amplitude.com/2/httpapi",
+        new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")
+      );
 
-            // Send POST request to Amplitude
-            var response = await client.PostAsync(
-                "https://api.amplitude.com/2/httpapi",
-                new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")
-            );
+      if (!response.IsSuccessStatusCode) {
+        throw new Exception("Response failed while logging to amplitude");
+      }
 
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception("Response failed while logging to amplitude");
-            }
-
-            Console.WriteLine("Event logged successfully");
-        }
-    } 
+      Console.WriteLine("Event logged successfully");
+    }
+  }
 }
