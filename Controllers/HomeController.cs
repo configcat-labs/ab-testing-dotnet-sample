c674170bffa7e0be043db0c9dd3e83c5ad4fdc71
Chavez Harris <74829200+codedbychavez@users.noreply.github.com>
2026-08-09 18:48:39 -0400

    import and use ConfigCat client in `HomeController.cs`

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,28 @@
+using ConfigCat.Client; // Import types from the ConfigCat SDK's namespace
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ab_testing_dotnet_sample.Models;
 
 namespace ab_testing_dotnet_sample.Controllers;
 
-public class HomeController : Controller
+// Inject the ConfigCat client via a constructor parameter
+// so we can use it within the Views/Home/index.cshtml file to control the "Add to Cart" button
+public class HomeController(IConfigCatClient configCatClient) : Controller
 {
-    public IActionResult Index()
+    public async Task<IActionResult> IndexAsync()
     {
-        return View();
+        // A unique user id is required when creating a ConfigCat User Object
+        var configCatUser = new User("user-id-123")
+        {
+            Email = "john@example.com",
+            Country = "United Kingdom",
+        };
+
+        // Get the flag's latest value
+        var isMyFeatureFlagEnabled = await configCatClient.GetValueAsync("myFeatureFlag", false, configCatUser);
+
+        // Return its value to the view via the view Model
+        return View(new IndexViewModel { IsMyFeatureFlagEnabled = isMyFeatureFlagEnabled });
     }
 
     public IActionResult Privacy()
